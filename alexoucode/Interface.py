5cecaab4eb0c1a3110a7a12f585ba7dfc6082623
Cat_x <gaildratalexis058041@gmail.com>
2026-01-05 13:06:39 +0100

    j'ai rajouté le système de vie avec son affichage et test de l'affichage du score

--- alexoucode/Interface.py
+++ alexoucode/Interface.py
@@ -18,8 +18,8 @@ vert_mort = (94, 229, 77)
     # initialiser le module display
 pygame.display.init()
     # dimensions de la fenêtre
-longueur = 800
-largeur = 600
+LARGEUR = 800
+HAUTEUR = 600
     # mettre un intitulé à la fenêtre
 pygame.display.set_caption("⋊ Space Invader ⋉")
     # mettre une icône pour la fenêtre
@@ -27,29 +27,35 @@ icon_home = pygame.image.load('../assets/icon_home.ico')
 pygame.display.set_icon(icon_home)
     # mettre un fond
 bg = pygame.image.load('../assets/fond-ecran_accueil.png')
-bg = pygame.transform.scale(bg, (longueur, largeur))
+bg = pygame.transform.scale(bg, (LARGEUR, HAUTEUR))
+    # image
+vie_0 =pygame.image.load("../assets/vie_0.png")
+vie_1 =pygame.image.load("../assets/vie_1.png")
+vie_2 =pygame.image.load("../assets/vie_2.png")
+vie_3 =pygame.image.load("../assets/vie_3.png")
     # variable qui permet de garder le jeu ouvert
 running = True
     # musique
 pygame.mixer.music.load("../assets/musique.mp3")
-pygame.mixer.music.play(-1)
+pygame.mixer.music.play(-1) # jouer la musique indéfiniment
 son_mort = pygame.mixer.Sound("../assets/son_mort.mp3")
     # police
 police_titre = pygame.font.Font("../assets/police_pixels.ttf", 72) #(chemin, taille)
 police_bouton = pygame.font.Font("../assets/police_pixels.ttf", 36)
-police_normale = pygame.font.Font("../assets/police_pixels.ttf", 30)
+police_mort = pygame.font.Font("../assets/police_pixels.ttf", 150)
+police_mort_score = pygame.font.Font("../assets/police_pixels.ttf", 75)
 
 # initialiser une fenêtre ou un écran pour display
 ecran = pygame.display.set_mode( 
-    size=(longueur, largeur),
+    size=(LARGEUR, HAUTEUR),
     flags= pygame.SHOWN, #créer une fenêtre affichée en mode visible
     depth=0, 
     display=0, 
     vsync=1 # avoir la syncronisation verticale
     )
+score = 15
+nb_vie = 1 # nombre de vies de base
 
-
-d = True
 while running:     
     ecran.blit(bg, (0, 0)) # mettre l'image en fond d'écran de l'accueil
 
@@ -58,15 +64,19 @@ while running:
             running = False # on sort de la boucle
         if event.type == pygame.KEYDOWN: # si on appuie sur une touche
             if event.key == pygame.K_ESCAPE: # si on appuie sur la touche "Esc"
-                running = False # on sort de la boucle
-        if event.type == pygame.WINDOWMOVED: # si on bouge la fenêtre
-            ecran.fill(vert)
-    if d == True :
+                running = False
+    ecran.blit(
+        pygame.transform.scale(pygame.image.load(f"../assets/vie_{nb_vie}.png"), (75, 75)),
+        (5, 0)
+        )
+    if nb_vie <= 0 :
         pygame.mixer.music.stop()
-        pygame.mixer.Sound.play(son_mort)
+        pygame.mixer.Sound.play(son_mort, 0)
         ecran.fill(vert_mort)
-        text_mort = police_normale.render('GAME OVER !!!!', False, rouge, (0, 0, 0))
-        ecran.blit(text_mort, (0,0))
+        text_mort = police_mort.render('GAME OVER !!!!', False, rouge)
+        text_mort_score = police_mort_score.render(f'Ton score est de : {score}', False, blanc)
+        ecran.blit(text_mort, (180,200))
+        ecran.blit(text_mort_score, (180,300))
     ips.tick(60) # taux de rafraichissement de l'image à 60 images par secondes
 
     pygame.display.update()
