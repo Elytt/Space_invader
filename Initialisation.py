087e59f6f80f5c0188ea910090ad3eaf01ec4b08
Elytt <elyttcprm7@gmail.com>
2026-01-11 17:03:35 +0100

    correctif

--- Initialisation.py
+++ Initialisation.py
@@ -230,6 +230,7 @@ def main():
             if p_ennemi.rect.colliderect(joueur):
                 print("DÉFAITE : Le vaisseau a été abimé !")
                 NB_VIE -= 1
+                p_ennemi.actif = False
 # Nettoyage des projectiles ennemis hors écran
         projectiles_ennemis[:] = [p for p in projectiles_ennemis if p.actif]
         #end nettoyage
